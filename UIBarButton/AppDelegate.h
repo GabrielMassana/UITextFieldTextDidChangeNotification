17736a628aff2de0a0ad3facd174ea8eecf045d7
Your Name HereGabrielMassana <gabrielmassana@gmail.com>
2014-04-02 20:06:10 +0100

    Initial Commit

---
+++ UIBarButton/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UIBarButton
+//
+//  Created by Jose Antonio Gabriel Massana on 02/04/14.
+//  Copyright (c) 2014 GabrielMassana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UIBarButtonTests/UIBarButtonTests.m
@@ -0,0 +1,34 @@
+//
+//  UIBarButtonTests.m
+//  UIBarButtonTests
+//
+//  Created by Jose Antonio Gabriel Massana on 02/04/14.
+//  Copyright (c) 2014 GabrielMassana. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UIBarButtonTests : XCTestCase
+
+@end
+
+@implementation UIBarButtonTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
